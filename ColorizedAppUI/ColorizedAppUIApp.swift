b4ef3e471278f6ce2630c38ba0888f6fc6d1b12d
ILKA777 <iomorin@edu.hse.ru>
2023-01-15 11:55:04 +0300

    Initial Commit

---
+++ ColorizedAppUI/ColorizedAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppUIApp.swift
+//  ColorizedAppUI
+//
+//  Created by Илья Морин on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
